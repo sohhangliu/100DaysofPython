f482a8a69f48d9512588b36dd8addb30821cf70b
Soh Hang Liu <sohhang.liu@gmail.com>
2023-09-02 20:22:04 -0400

    Some progress

--- Day17: Quiz Project and the Benefits of OOP/main.py
+++ Day17: Quiz Project and the Benefits of OOP/main.py
@@ -1,6 +1,6 @@
 from question_model import Question
 from data import question_data
-
+from quiz_brain import QuizBrain
 question_bank = []
 
 for question_number in question_data:
@@ -9,3 +9,6 @@ for question_number in question_data:
     question = Question(text, answer)
     question_bank.append(question)
 
+quiz = QuizBrain(question_bank)
+while quiz.stillhasquestions():
+    quiz.nextquestion()
--- Day17: Quiz Project and the Benefits of OOP/quiz_brain.py
+++ Day17: Quiz Project and the Benefits of OOP/quiz_brain.py
@@ -1,3 +1,26 @@
 #TODO: asking the questions
 #TODO: checking if answer was correct
 #TODO: checking if we're the end of the quiz
+
+class QuizBrain:
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.question_list = question_list
+        self.score = 0
+
+    def nextquestion(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q{self.question_number}: {current_question.text} (True/False)? ")
+        self.check_answer(user_answer, current_question.answer)
+    def stillhasquestions(self):
+        return self.question_number <= len(self.question_list)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print("You got it right!")
+        else:
+            print("You got it wrong.")
+        print(f"The correct answer was: {correct_answer}")
+        print(f"Your score is {self.score} \n")
